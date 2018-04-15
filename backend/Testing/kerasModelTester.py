53e4fb97585b85faf92d8e250ba2ac315fc49525
Wiss Ben <wissben69@gmail.com>
2018-04-16 00:28:28 +0100

    CHANGING :  Adjusting code and fixing bugs
    Signed-off-by: Wiss Ben <wissben69@gmail.com>

--- backend/Testing/kerasModelTester.py
+++ backend/Testing/kerasModelTester.py
@@ -5,12 +5,13 @@ from tensorflow.python.keras.models import load_model
 
 def fileToArray(path):  # Here you may redifine how u read from your file
     fileReader = open(path, 'r')
-    data = read_csv(path, header=None).replace('?', 0)
+    data = read_csv(path, header=None).replace('?', 0).astype('float')
     values = data.values
-    np.random.shuffle(values)
+    # np.random.shuffle(values)
 
-    dataTestSize = 1500
-    return values[0:dataTestSize, 2:38]
+    dataTestSize = 10000
+    # return data.values[1500:1501,2:38]
+    return values[0:dataTestSize, 2:38], values[0:dataTestSize, 37:42]
 
 
 def loadModel(pathToModel):
@@ -20,11 +21,16 @@ def loadModel(pathToModel):
 
 
 pathToTestinFile = "/home/wiss/CODES/TP-AARN/Mini-Project/DataSets/dataOneOf5.csv"
-pathToModel = "/home/wiss/CODES/TP-AARN/Mini-Project/backend/training/neuralNetworkModel/Models/Weigths/modelTest.hd5"
+pathToModel = "/home/wiss/CODES/TP-AARN/Mini-Project/backend/training/Models/Weigths/modelTest.hd5"
 batchSize = 100
 resultPath = "./results.csv"
 
-testingSet = fileToArray(pathToTestinFile)
+testingSet, targetSet = fileToArray(pathToTestinFile)
 model = load_model(pathToModel)
-score = model.predict(testingSet, batchSize)
-print(score)
+Y = model.predict_proba(testingSet)
+matching = []
+for i, j in zip(Y, targetSet):
+    matching.append([np.unravel_index(i.argmax(), i.shape)[0] == np.unravel_index(j.argmax(), j.shape)[0]])
+unique_elements, counts_elements = np.unique(matching, return_counts=True)
+print("Frequency of unique values of the said array:")
+print(np.asarray((unique_elements, counts_elements)))
--- backend/training/kerasModel.py
+++ backend/training/kerasModel.py
@@ -1,3 +1,5 @@
+import time
+
 import numpy as np
 from pandas import read_csv
 from sklearn.preprocessing import Imputer
@@ -5,22 +7,22 @@ from tensorflow.python.keras.callbacks import TensorBoard
 from tensorflow.python.keras.layers import Dense
 from tensorflow.python.keras.models import Sequential
 
-stats = open("Models/stats.csv", 'w')
-stats.write("layers,tranFnc,opt,epochs,batch,score,loss\n")
+statsFile = "Models/trainingStats.csv"
+stats = open(statsFile, 'w')
+stats.write("layers,tranFnc,opt,epochs,batch,accuracy,mse,trainingTime\n")
+stats.close()
 file = "/home/wiss/CODES/TP-AARN/Mini-Project/DataSets/dataOneOf5.csv"
 # SIZE OF INPUT DATASET TO TRAIN ON (MAX IS 78071)
-MAX = 1000
+MAX = 78071
 trainingRows = int(MAX * 75 / 100)
 testingRows = MAX - trainingRows
 
 
-def replaceMissingData(X, values):
+def replaceMissingData(X):
     # fill missing values with mean column values
     imputer = Imputer()
     transformed_X = imputer.fit_transform(X)
-    transformed_values = imputer.fit_transform(values)
     # count the number of NaN values in each column
-    print(np.isnan(transformed_values).sum())
     return transformed_X
 
 
@@ -28,6 +30,7 @@ def evaluateModel(model, X, Y, batch):
     score = model.evaluate(X, Y, batch_size=batch, verbose=1)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
+    print('Test MSE :', score[2])
     # sleep(25)
     return score
 
@@ -46,28 +49,34 @@ def trainModel(modelName, train_X, train_Y, test_X, test_Y, layers, inputDime, o
     model.add(Dense(outputDim, activation='softmax'))
 
     # Compile model
-    model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
-
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy', 'mse'])
+    start_time = time.time()
     # Fit the model
-    model.fit(train_X, train_Y, epochs=epochs, batch_size=batch, callbacks=[tb], verbose=1, validation_split=0.15,
+    model.fit(train_X, train_Y, epochs=epochs, batch_size=batch, callbacks=[tb], verbose=1, validation_split=0.10,
               shuffle=True)
 
     # evaluate the model
     scores = evaluateModel(model, test_X, test_Y, batch)
+    elapsed_time = time.time() - start_time
 
     # Saving the model
     architecture = ""
     for layer in layers:
         architecture = architecture + str(layer) + "-"
     architecture = architecture[0:len(architecture) - 1]
+
+    stats = open(statsFile, 'a')
     stats.write(architecture + "," +
                 transfnc + "," +
                 optimizer + "," +
                 str(epochs) + "," +
                 str(batch) + "," +
-                str(scores[1] * 100) +
-                "," + str(scores[0]) + "\n")
-
+                str(scores[1] * 100) + "," +
+                str(scores[2]) + "," +
+                (time.strftime("%H:%M:%S.", time.gmtime(elapsed_time))) +
+                "\n")
+    print(time)
+    stats.close()
     # serialize model to JSON
     model_json = model.to_json()
     with open("Models/JSON/" + modelName + "_Architecure.json", "w") as json_file:
@@ -76,6 +85,7 @@ def trainModel(modelName, train_X, train_Y, test_X, test_Y, layers, inputDime, o
     model.save("Models/Weigths/" + modelName + ".hd5")
     print("SAVING " + modelName + " TO DISK")
     print("FINISHED TRAINING MODEL : " + modelName)
+
     # sleep(30)
     return model
 
@@ -85,39 +95,41 @@ def trainModel(modelName, train_X, train_Y, test_X, test_Y, layers, inputDime, o
 ##
 data = read_csv(file, header=None).replace('?', 0)
 values = data.values
+np.random.seed(7)
 np.random.shuffle(values)
+# values=replaceMissingData(values)
+
 
 training_X = values[0:trainingRows, 2:38]
 training_Y = values[0:trainingRows, 37:42]
-transformedTraining_X = replaceMissingData(training_X, data.values)
-print(training_Y)
 
 # print(values[trainingRows+1])
 testing_X = values[trainingRows + 1:MAX, 2:38]
 testing_Y = values[trainingRows + 1:MAX, 37:42]
-transformedTesting_X = replaceMissingData(testing_X, data.values)
-# print(transformed_X[2])
 
-
-#training the model
-model = trainModel("modelTest", transformedTraining_X, training_Y, transformedTesting_X, testing_Y, [50, 25, 15], 36, 5,
-                   'relu', 'sgd', 150, 100)
-
+# training the model
+model = trainModel("modelTest", training_X, training_Y, testing_X, testing_Y, [120, 120, 120], 36, 5,
+                   'relu', 'Adadelta', 150, 100)
+#
+# del model
+# model = load_model(
+#     "/home/wiss/CODES/TP-AARN/Mini-Project/backend/training/neuralNetworkModel/Models/Weigths/modelTest.hd5")
+# print(model.predict(testing_X, batch_size=128))
 # #
 # Uncomment to launch multiple models training
-# transFncs = ['relu', 'tanh', 'sigmoid']
+# transFncs = ['sigmoid','relu', 'tanh']
 # optimizers = ['sgd', 'adagrad', 'adam']
 # inDim = 36
 # outDim = 5
 # batch = 125
 # epochs = 150
-# modelBaseNam= "model_"
+# modelBaseName = "model_"
 # for tranFnc in transFncs:
 #     for opt in optimizers:
 #         # layers =[]
-#         for i in range(40, 51):
-#             for j in range(20, 26):
-#                 for k in range(10, 15):
+#         for i in range(20, 51, 5):
+#             for j in range(10, 26, 5):
+#                 for k in range(5, 16, 5):
 #                     modeName = modelBaseName + \
 #                                str(i) + "_" + \
 #                                str(j) + "_" + \
@@ -126,9 +138,6 @@ model = trainModel("modelTest", transformedTraining_X, training_Y, transformedTe
 #                                opt + "_" + str(batch)
 #                     layers = [i, j, k]
 #                     print("TRAINING MODEL  :" + modeName)
-#                     trainModel(modeName, transformedTraining_X, training_Y, transformedTesting_X, testing_Y, layers,
+#                     trainModel(modeName, training_X, training_Y, testing_X, testing_Y, layers,
 #                                inDim, outDim, tranFnc, opt, epochs, batch)
-
-
-stats.close()
-
+#
